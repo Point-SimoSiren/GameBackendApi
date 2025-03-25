c9a77816fbaeacef3fa3101e8df5096e9cca8506
Simppa <simo.siren@careeria.fi>
2025-03-25 10:58:01 +0200

    Genret listaus ja uudenluonti

---
+++ GameBackendApi/Controllers/GenresController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using GameBackendApi.Models; // Lisätty
+
+namespace GameBackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        // Tietokantayhteys
+        private readonly GameDbContext db = new GameDbContext();
+
+
+        // Hakee kaikki genret
+        [HttpGet]
+        public ActionResult GetGenres()
+        {
+            var genres = db.Genres.ToList();
+
+            return Ok(genres);
+        }
+
+
+        // Lisää uuden genren
+        [HttpPost]
+        public ActionResult AddNewGenre([FromBody] Genre genre)
+        {
+            db.Genres.Add(genre);
+            db.SaveChanges();
+
+            return Ok($"lisätty uusi genre: {genre.GenreName}.");
+        }
+
+    }
+}
